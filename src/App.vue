8322f773ee4a2382c5228e8d47aff838047754e5
Sophat <leatsophat02@gmail.com>
2021-01-21 10:11:58 +0600

    add

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  
+  <HelloWorld title="IS" msg="Dear Seakthong.Queen.Vue.js  " />
+  <!-- <FirstVue msg="Dec 22, 2020" /> -->
+  <!-- <FirstVue msg="MY WORLD IN MY HEAD" /> -->
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+// import FirstVue from "./components/first.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    // FirstVue,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/first.vue
@@ -0,0 +1,15 @@
+<template>
+  <div><h1>Seakthong.Queen.JS</h1></div>
+  <div>
+    <h3>Start On : {{ msg }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SQ",
+  props: {
+    msg: String,
+  },
+};
+</script>
